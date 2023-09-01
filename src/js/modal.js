1e5265d257091c274bb9f2312bb537a85a6159ca
ryzhay <elenaaltukhova1@gmail.com>
2023-09-01 22:25:29 +0200

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -8,3 +8,15 @@ const refs = {
     backdrop: document.querySelector('.js-backdrop'),
 };
 
+refs.closeModalBtn.addEventListener('click', onCloseModal);
+refs.backdrop.addEventListener('click', onBackdropClic);
+
+function onCloseModal() {
+    document.body.classList.remove('show-modal');
+};
+function onBackdropClic(event) {
+if(event.currentTarget === event.Target){
+    onCloseModal();
+}
+}
+
