cf08071833aaf7cd588c365023c86d1d8572599f
Artur-man <123097639+arthyr-men@users.noreply.github.com>
2023-09-01 09:19:27 +0200

    support

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/categories';
+import './js/gallery';
+import './js/header';
+import './js/modal';
+import './js/onecategory';
+import './js/shopping';
+import './js/support';
+import './js/theme';
