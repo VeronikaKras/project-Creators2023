22db688aa345b0346e800e5cd9f9e42fcfdf0a49
ryzhay <elenaaltukhova1@gmail.com>
2023-09-01 17:42:16 +0200

    update

---
+++ src/js/modal.js
@@ -0,0 +1,10 @@
+// https://books-backend.p.goit.global/books/bookId
+// import axios from 'axios';
+// axios.defaults.baseURL = 'https://books-backend.p.goit.global/books';
+
+const refs = {
+    // openModal: document.querySelector();
+    closeModalBtn: document.querySelector('[data-action = "close-modal"]'),
+    backdrop: document.querySelector('.js-backdrop'),
+};
+
