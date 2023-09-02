dc061c60529f14cf7727a74b24fba69738d4b4d1
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2023-09-02 11:00:11 +0200

    update css

--- src/js/shopping.js
+++ src/js/shopping.js
@@ -7,15 +7,52 @@ import bookshop1x from '../img/shopping/bookshop-1x.jpg'
 import bookshop2x from '../img/shopping/bookshop-2x.jpg'
 
 
+////// пробую по запросу малювати розмітку
 
+import axios from 'axios';
+// import Notiflix from 'notiflix';
+axios.defaults.baseURL = 'https://books-backend.p.goit.global/books'; 
+// const booksSection = document.querySelector('.gallery');
+fetchBooks();
+// async function fetchBooks() {
+//   const { data } = await axios.get('{1}');
+//   try {
+//     const markup = renderMarkup(data);
+//    cardList.innerHTML = markup; 
+//   } catch (error) {
+//     console.log(error.message);
+//     // Notiflix.Notify.failure('Something went wrong');
+//     console.log('Something went wrong:', error.message);
+//   }
+// }
+
+async function fetchBooks() {
+  try {
+    const response = await axios.get('/1');
+    if (response.status === 200) {
+      const markup = renderMarkup(response.data);
+      cardList.innerHTML = markup;
+    } else {
+      console.log('Received a non-200 status code:', response.status);
+      // Обработка ошибки, например, Notiflix.Notify.failure()
+    }
+  } catch (error) {
+    console.log(error.message);
+    console.log('Something went wrong:', error.message);
+    // Обработка ошибки, например, Notiflix.Notify.failure()
+  }
+}
+
+
+///////
 const cardList = document.querySelector('.card-list');
 
 
-const savedBooks = JSON.parse(localStorage.getItem('saved-books-in-modal')) ?? [];
+// const savedBooks = JSON.parse(localStorage.getItem('saved-books-in-modal')) ?? [];
 
 
 
-CreateMarkup(savedBooks);
+// CreateMarkup(savedBooks);
 
 //Рендеримо розмітку для книг
 
@@ -112,3 +149,75 @@ function emptyLocaleMarkup() {
 }
 
 
+
+
+
+/////// експеримент 
+
+
+function renderMarkup(arr) {
+
+
+    // перевіряємо, чи є щось у localStotrage - рендеримо розмітку
+    // якщо порожньо - інша розмітка в блоці else
+return arr.map(({ id, book_image, title, list_name, description, author, buy_links }) =>
+            `<li class="shop-card" data-id="${id}">
+  <img src="${book_image}" alt="book cover" />
+  <div class="shop-card-details">
+    <h2 class="shop-card-title">${title}</h2>
+    <p class="shop-card-category">${list_name}</p>
+    <p class="shop-card-description">${isAvaliableDescription(description)}</p>
+    <p class="shop-card-author">${author}</p>
+    <button class="basket">
+    <svg class="trash-btn" width="18" height="18">
+  <use href="${svgIconTrash}"></use>
+</svg>
+</button>
+    <li class="shop-card-buy-links">
+        <a
+            href="${buy_links[0].url}"
+            class="shop-card-link"
+            target="blank"
+          >
+            <img
+              class="buy-links-icon"
+              src="${amazon2x}" srcset="${amazon1x} 1x, ${amazon2x} 2x" 
+              width="48"
+              alt="platform-icon"
+            />
+          </a>
+      </li>
+      <li class="shop-card-buy-links">
+          <a
+            href="${buy_links[1].url}"
+            class="shop-card-link"
+            target="blank"
+          >
+            <img
+              class="buy-links-icon"
+              src="${apple2x}" srcset="${apple1x} 1x, ${apple2x} 2x" 
+              width="28"
+              alt="platform-icon"
+            />
+          </a>
+        </li>
+     <li class="shop-card-buy-links">
+          <a
+            href="${buy_links[4].url}"
+            class="shop-card-link"
+            target="blank"
+          >
+            <img
+              class="buy-links-icon"
+             src="${bookshop2x}" srcset="${bookshop1x} 1x, ${bookshop2x} 2x" 
+              width="32"
+              alt="platform-icon"
+            />
+          </a>
+        </li>
+    </li>
+  </div>
+</li>`)
+     .join('');
+      // cardList.innerHTML = renderMarkup; 
+}
