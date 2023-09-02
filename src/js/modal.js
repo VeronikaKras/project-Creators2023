00ebbc92b2e2671fb1f36b0db7ade53093be871c
ryzhay <elenaaltukhova1@gmail.com>
2023-09-02 18:59:40 +0200

    modal update

---
+++ src/js/modal.js
@@ -0,0 +1,55 @@
+// import axios from 'axios';
+// axios.defaults.baseURL = 'https://books-backend.p.goit.global/books';
+
+// //   function onOpenModal(e) {
+// //     e.preventDefault()
+// //     console.log(e)
+// //     console.log(e.target.closest('li').id)
+// //     fetchBooksById(e.target.closest('li').id)
+// //     modal.classList.toggle('visually-hidden')
+// // }
+  
+
+
+
+// const refs = {
+//     openModal: document.querySelector(''),
+//     closeModalBtn: document.querySelector('[data-action = "close-modal"]'),
+//     backdrop: document.querySelector('.js-backdrop'),
+//     fetchBooksById: document.querySelector('.gallery'),
+// };
+// refs.openModal.addEventListener('click', onOpenModal);
+// refs.closeModalBtn.addEventListener('click', onCloseModal);
+// refs.backdrop.addEventListener('click', onBackdropClick);
+// refs.fetchBooksById.addEventListener('click', onBookClick);
+// function onOpenModal() {
+//     window.addEventListener('keydown', onEscKey);
+//     document.body.classList.add('show-modal');
+// }
+
+// function onOpenModal(e) {
+//    e.preventDefault()
+//     window.addEventListener('keydown', onEscKey);
+//     document.body.classList.add('show-modal');
+  
+// }
+
+// function onCloseModal() {
+//     window.removeEventListener('keydown', onEscKey);
+//     document.body.classList.remove('show-modal');
+  
+// };
+// function onBackdropClick(event) {
+// if(event.currentTarget === event.target){
+//     onCloseModal();
+// };
+// }
+
+// function onEscKey(event) {
+//     const ESC_KEY_CODE = 'Escape';
+//     if(event.code === ESC_KEY_CODE) {
+//         onCloseModal()
+        
+//     }
+// } 
+
