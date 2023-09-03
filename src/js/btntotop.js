52474692ff9462065274570123a5abe10e05aea2
SKhrebtan <shrebtan@gmail.com>
2023-09-03 21:42:22 +0300

    btntotop done

---
+++ src/js/btntotop.js
@@ -0,0 +1,13 @@
+const myButton = document.getElementById("btntotop");
+myButton.addEventListener('click', onPageTop);
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    myButton.style.display = "block";
+  } else {
+    myButton.style.display = "none";
+  }};
+
+function onPageTop() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+};
