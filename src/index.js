fe88e0195114f7a8b2dbddb82fc0466c9fa25b7e
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2023-09-04 02:01:24 +0200

    update js+css+modal

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import './js/header';
 import './js/modal';
 import './js/home-main';
-import './js/shopping';
+import './js/shopping-list';
 import './js/support';
 import './js/theme';
 import './js/pagination';
