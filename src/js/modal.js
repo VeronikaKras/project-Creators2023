7f4b7f9b0c2b63b56173f55a37649447ed615944
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2023-09-04 09:40:41 +0200

    update_modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,4 +1,12 @@
 import axios from 'axios';
+
+import amazon1 from '../img/shopping/amazon1.png'
+import amazon2 from '../img/shopping/amazon2.png'
+import apple1 from '../img/shopping/apple1.png'
+import apple2 from '../img/shopping/apple2.png'
+import bookshop1 from '../img/shopping/bookshop1.png'
+import bookshop2 from '../img/shopping/bookshop2.png'
+
 axios.defaults.baseURL = 'https://books-backend.p.goit.global/books';
   const refs = {
       gallery: document.querySelector('.gallery'),
@@ -11,6 +19,7 @@ async function fetchBooksById(id) {
     const { data } = await axios.get(`/${id}`);
     const markup = modalRender(data);
     refs.modal.innerHTML = markup;
+   
     }
 refs.gallery.addEventListener('click', onOpenModal);
 refs.galleryContainer.addEventListener('click', onOpenModal);
@@ -57,12 +66,54 @@ function modalRender(data) {
     console.log(book)
     return (
         `
-        img src="${book_image}">
+        <img class="modal-img" src="${book_image}"/>
         <h2>${author}</h2>
          <h3>${publisher}</h3>
          <p>${list_name}</p>
          <p>${title}</p>
-         <button type = "button" class = "close-modal">x</button>
+          <ul class="buy-links">
+    <li class="buy-links">
+      <a
+             href="${buy_links[0].url}"
+            class="shop-card-link"
+            target="blank"
+          >
+            <img
+              class="buy-links-icon amazon"
+              src="${amazon2}" srcset="${amazon1} 1x, ${amazon2} 2x"
+              alt="amazon-icon"
+            />
+          </a>
+      </li>
+      <li class="shop-card-buy-links">
+         <a
+            href="${buy_links[1].url}"
+            class="shop-card-link"
+            target="blank"
+          >
+            <img
+              class="buy-links-icon apple"
+              src="${apple2}" srcset="${apple1} 1x, ${apple2} 2x"
+              
+              alt="apple-icon"
+            />
+          </a>
+        </li>
+     <li class="shop-card-buy-links">
+          <a
+            href="${buy_links[4].url}"
+            class="shop-card-link"
+            target="blank"
+          >
+            <img
+              class="buy-links-icon bookstore"
+             src="${bookshop2}" srcset="${bookshop1} 1x, ${bookshop2} 2x"  
+             
+              alt="bookstore-icon"
+            />
+          </a>
+        </li>
+        </ul>
          <button type = "button" class = "js-addButton" id="${book}">Add to shopping list</button>
         `
 )
