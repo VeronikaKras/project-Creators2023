01751db27e7698fb250eb7d066fe86fe548bd174
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2023-09-05 22:37:12 +0200

    changes after19

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,12 +1,12 @@
-import Pagination from 'tui-pagination'; 
+// import Pagination from 'tui-pagination';
 
 const tuiContainer = document.querySelector('.tui-pagination');
-let allBooks = JSON.parse(localStorage.getItem('saved-books-in-modal'))
+let allBooks = JSON.parse(localStorage.getItem('saved-books-in-modal'));
 if (!allBooks) {
   allBooks = 1;
 }
 const options = {
-  totalItems: allBooks.length  ,
+  totalItems: allBooks.length,
   itemsPerPage: 3,
   page: 1,
   centerAlign: false,
@@ -14,20 +14,21 @@ const options = {
   lastItemClassName: 'tui-last-child',
   template: {
     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
     moveButton:
       '<a href="#" class="tui-page-btn tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</a>',
     disabledMoveButton:
       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
       '</span>',
     moreButton:
       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-        '<span class="tui-ico-ellip">...</span>' +
-      '</a>'
-  }
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
 };
 
 export const pagination = new Pagination(tuiContainer, options);
